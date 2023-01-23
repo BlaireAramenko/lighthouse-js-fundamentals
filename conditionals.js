b3f37b2aba2f17408aebebdf96b05fb7df020b44
Blaire Aramenko <blaire.aramenko21@gmail.com>
2023-01-22 19:34:27 -0800

    more practice

--- conditionals.js
+++ conditionals.js
@@ -37,3 +37,78 @@ while (x <= 20) {
     // increment x at the end of each loop
     x = x + 1 ; 
 }
+
+
+var num = 99;
+
+// while loop with a stop condition
+while (num <= 20) {
+    if (x % 3 === 0 && x % 5 === 0) {
+        console.log("JuliaJames");
+    }else if (x % 5 === 0) {
+        console.log("James");
+    }else if (x % 3 === 0) {
+        console.log("Julia");
+    }
+    else {
+        console.log(x);
+    }
+    // increment x at the end of each loop
+    x = x + 1 ; 
+}
+
+var num = 99;
+
+while (num >=1) {
+    // Last iteration. Note occurances of bottle, bottle, bottleS
+    if (num === 1) {
+        console.log(num + " bottle of juice on the wall! "
+                    + num + " bottle of juice! Take one down, pass it around... "
+                    + (num-1) + " bottles of juice on the wall!");
+    }
+    // Second-last iteration. Note occurances of bottleS, bottleS, bottle
+    else if (num === 2){
+        console.log(num + " bottles of juice on the wall! "
+                    + num + " bottles of juice! Take one down, pass it around... "
+                    + (num-1) + " bottle of juice on the wall!");
+    }
+    // All other iterations. Note occurances of bottleS, bottleS, bottleS
+    else {
+        console.log(num + " bottles of juice on the wall! "
+                    + num + " bottles of juice! Take one down, pass it around... "
+                    + (num-1) + " bottles of juice on the wall!");
+    }
+    num = num - 1;
+}
+
+// Let us take an iteration variable, that represent the remaining Time in seconds
+n = 60
+
+// While loop with a stop condition
+while (n >= 0) {
+    if (n === 50) {
+        console.log("Orbiter transfers from ground to internal power");
+    }
+    else if (n === 31) {
+        console.log("Ground launch sequencer is go for auto sequence start");
+    }
+    else if (n === 16) {
+        console.log("Activate launch pad sound suppression system");
+    }
+    else if (n === 10) {
+        console.log("Activate main engine hydrogen burnoff system");
+    }
+    else if (n === 6) {
+        console.log("Main engine start");
+    }
+    else if (n === 0) {
+        console.log("Solid rocket booster ignition and liftoff!");
+    }
+    else{
+        console.log("T-"+n+" seconds");
+    }
+
+    //Never forget to decrement/increment the iteration variable in a while loop
+    // Otherwise, you loop will run infinite iterations
+    n = n-1;
+}
