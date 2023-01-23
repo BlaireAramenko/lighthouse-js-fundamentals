e14ef8a54bde1d2bee9559c38dc9446242cdb28f
Blaire Aramenko <blaire.aramenko21@gmail.com>
2023-01-22 19:14:32 -0800

    practice

--- conditionals.js
+++ conditionals.js
@@ -10,3 +10,30 @@ if (temperature < 0) {
 
 console.log("Now you're ready to go outside!");
 
+const whichSchool = function(age) {
+
+if (age < 13) {
+  console.log("Elementary School");
+} else if (age > 13) {
+  console.log("Secondary School");
+} else {
+  console.log("Lighthouse Labs");
+}
+
+var x = 1;
+
+// while loop with a stop condition
+while (x <= 20) {
+    if (x % 3 === 0 && x % 5 === 0) {
+        console.log("JuliaJames");
+    }else if (x % 5 === 0) {
+        console.log("James");
+    }else if (x % 3 === 0) {
+        console.log("Julia");
+    }
+    else {
+        console.log(x);
+    }
+    // increment x at the end of each loop
+    x = x + 1 ; 
+}
